9a1d45c1d0c98ebcb37eb18638c504486e0509ea
Maria do Carmo Correia <mcaac@cesar.school>
2018-10-26 15:24:57 -0300

    speed-sensor: Initial commit

---
+++ speed-sensor/cloud/backend/app.js
@@ -0,0 +1,31 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forwarding to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+// error handler
+
+app.use(function (err, req, res) {
+// set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+// render the error page
+
+module.exports = app;
