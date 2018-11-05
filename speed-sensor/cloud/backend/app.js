b5fe60762735e46a317dab9d09d5747e1a2ce961
Maria do Carmo Correia <mcaac@cesar.school>
2018-11-05 16:47:48 -0300

    speed-sensor: cloud: Authenticate user
    Authenticates the user for the speed sensor web app

--- speed-sensor/cloud/backend/app.js
+++ speed-sensor/cloud/backend/app.js
@@ -1,49 +1,35 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const favicon = require('static-favicon');
-const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+const devices = require('./routes/devices');
 
 const app = express();
 
-app.use(favicon());
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forwarding to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use('/devices', devices);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-// error handlers
+// error handler
+app.use(function (err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use((err, req, res) => {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res) => {
+  // render the error page
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.render('error');
 });
-
 
 module.exports = app;
