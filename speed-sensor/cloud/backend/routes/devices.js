7369aceb8f666ea2af75b4e9e10179a828b324e3
Maria do Carmo Correia <mcaac@cesar.school>
2018-11-05 16:47:48 -0300

    speed-sensor: cloud: Authenticate user
    Authenticates the user for the speed sensor web app

---
+++ speed-sensor/cloud/backend/routes/devices.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const KNoTCloud = require('knot-cloud');
+
+const router = express.Router();
+
+router.get('/', async function (req, res) {
+  const meshbluHost = req.get('Meshblu-Host');
+  const meshbluPort = parseInt(req.get('Meshblu-Port'), 10);
+  const meshbluAuthUUID = req.get('Meshblu-Auth-UUID');
+  const meshbluAuthToken = req.get('Meshblu-Auth-Token');
+
+  const cloud = new KNoTCloud(
+    meshbluHost,
+    meshbluPort,
+    meshbluAuthUUID,
+    meshbluAuthToken
+  );
+
+  try {
+    await cloud.connect();
+    res.status(200).send('OK');
+  } catch (err) {
+    res.status(500).send(err);
+    console.log('not connected!!!');
+  } finally {
+    await cloud.close();
+  }
+});
+
+module.exports = router;
